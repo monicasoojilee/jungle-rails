5fdc92fa6d6da2e8ba14b046b07e92ee7b883ea7
MonajiLee <lee.smonica@gmail.com>
2018-11-09 01:25:21 +0000

    Resolve bug with empty cart

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-    http_basic_authenticate_with name: ENV['ADMIN_AUTHENTICATE_USERNAME'], password: ENV['ADMIN_AUTHENTICATE_PASSWORD'], only: [:index, :new, :create]
+    http_basic_authenticate_with name: ENV["ADMIN_AUTHENTICATE_USERNAME"], password: ENV["ADMIN_AUTHENTICATE_PASSWORD"], only: [:index, :new, :create]
 
     def index
         @categories = Category.order(:name).all
