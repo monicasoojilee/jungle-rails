e2020ed910c5641ddebcf67cc30df7975327ddd1
MonajiLee <lee.smonica@gmail.com>
2018-11-10 04:59:44 +0000

    Implement user restricted review creation, and deletion

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+    before_filter :authorize
+
     def create
         @product = Product.find(params[:product_id]) 
         @review = Review.new(:product_id => params[:product_id],
@@ -13,6 +15,13 @@ class ReviewsController < ApplicationController
             redirect_to @product, notice: 'Review failed to submit!'
         end
     end
+
+    def destroy
+        @product = Product.find(params[:product_id])
+        @review = Review.find_by(product_id: @product.id, user_id: current_user.id)
+        @review.destroy
+        redirect_to @product, notice: 'Comment deleted!'
+      end
 
     private
     def review_params
