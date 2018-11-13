9b6537c2b29bec5efb88c4641a19f9fb06c987cb
MonajiLee <lee.smonica@gmail.com>
2018-11-13 20:28:56 +0000

    Add rspec test for navigating to product details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+  # SETUP (identical to /spec/home_page_spec.rb)
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product detail page by clicking on a product" do
+    # ACT
+    visit root_path
+    find_link(@category.products.first.name).click
+
+    # DEBUG / VERIFY
+    # save_screenshot
+    expect(page).to have_content("#{@category.products.first.name}", count: 2)
+  end
+
+
+
+end
